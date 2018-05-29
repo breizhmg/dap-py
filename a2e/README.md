7748d2e9efef52323ddba9c2993a6009c41a1005
logdog <logan.dihel@pnnl.gov>
2018-05-29 10:39:58 -0700

    Support for multi-dimensional variables

--- a2e/README.md
+++ a2e/README.md
@@ -1,45 +1,62 @@
 # a2e Package
+
 This package contains the A2e module and wraps all of the other packages to make importing modules easy. Each of the sub-packages are explained in detail in their own README files. For more information on the A2e module, keep reading.
 
-# A2e Module
+## A2e Module
+
 The A2e module is a high-level interface that allows the programmer to use our API to authenticate, search, and download the data they want in very few lines of code.
 
 ## Setup
+
 First, import the module:
-```
+
+```python
 from a2e import A2e
 ```
+
 Then, create an instance of the A2e class:
-```
+
+```python
 a2e = A2e.A2e()
 ```
+
 And that's it! Setup is complete. All future methods will revolve around this `a2e` object. Alternatively, the constructor accepts two optional arguments: `cert` and `quiet`. The cert argument takes in a cert string. The quiet argument when set to `True` disables output. Useful for scripting.
 
-## Authentication
+### Authentication
+
 Authentication is very simple with this module. This module supports both __basic__ and __certificate__ authentication protocols. The basic methods do not use a certificate, expire more quickly, and do not support two-factor authentication. The other methods in this module will not work without proper authentication. When an A2e instance is created, the constructor looks for any existing certificates in `~/.cert` and tries to renews it. Alternatively, a certificate can be passed into the constructor:
-```
+
+```python
 a2e = A2e.A2e(<cert>)
 ```
+
 If the certificate is valid, it will be renewed and written to the `~/.cert` file. Otherwise, it defaults to basic guest authentication. The following are the different authentication methods:
 
 #### `a2e.setup_guest_auth()`
+
 Sets up basic authentication for a guest user. This is identical to setting up basic authentication with the username and password `guest`.
 
 #### `a2e.setup_basic_auth(username=None, password=None)`
+
 Sets up basic authentication with a username and password. The arguments are optional, but the module will prompt for them if omitted.
 
 #### `a2e.setup_cert_auth(username=None, password=None)`
+
 Similar to the method above, but will request a certificate instead of using basic authentication. The certificate is stored in the `~./cert` file. Will prompt user for omitted arguments.
 
 #### `a2e.setup_two_factor_auth(username=None, password=None, email=None, authcode=None)`
+
 Similar to the method above, but uses two-factor authentication. The authcode is the 6 digit number from Google Authenticator. This is the highest authentication level available. The certificate is stored in the `~./cert` file. Will prompt user for omitted arguments.
 
 ### Searching for Files
+
 Searching is straight-forward with this module. Simple call this method and a list of files will be returned.
 
 #### `a2e.search(filter_arg, table='Inventory')`
+
 Query the respective table in AWS with the given filter argument. An example filter argument is shown below:
-```
+
+```python
 filter_arg = {
     'Dataset': 'wfip2/lidar.z01.b0',
     'date_time': {
@@ -48,19 +65,24 @@ filter_arg = {
     'file_type': 'nc',
 }
 ```
+
 The documentation for constructing the filter argument can be found [here](https://github.com/a2edap/tools/tree/master/lambda/api/data-download).
 
-## Downloading Files
+### Downloading Files
+
 There are two ways to download files using this module. The first places an order for the files, gets the download urls, and downloads the files to the provided path. The second uses the `/downloads` api method to search for the files and download them to the provided path in one step. The latter is dangerous if you don't know how many files you are about to download!
 
 #### `a2e.download_files(files, path='/var/tmp/', force=False)`
+
 Provided with a list of files, place an order for the files and download them. The path specifies the directory the files will download to, and the force flag determines whether files will be overriden. By default, if a file already exists, it will not be downloaded. Does not work using guest credentials because guests cannot place an order.
 
 #### `a2e.download_search(filter_arg, path='/var/tmp/', force=False)`
+
 Provided with a [filter argument](https://github.com/a2edap/tools/tree/master/lambda/api/data-download), search the Inventory table and download the files in s3. I heard a rumor through the grapevine that only files in s3 will be downloaded, so if you think some data could be someone else, use the other download method.
 
 ## Example
-```
+
+```python
 from A2e import A2e
 
 a2e = A2e.A2e()
--- a2e/plot/plot.py
+++ a2e/plot/plot.py
@@ -2,7 +2,7 @@
 File: plot.py
 Authors: Logan Dihel
 Date: 5/25/2018
-Last Modified: 5/25/2018
+Last Modified: 5/29/2018
 Description: This module is a high-level interface
 for manipulating and graphing netcdf4 files.
 '''
@@ -10,7 +10,6 @@ for manipulating and graphing netcdf4 files.
 import os
 import re
 import sys
-import copy
 import numpy as np
 from netCDF4 import Dataset
 from netCDF4 import MFDataset
@@ -51,11 +50,199 @@ class Plotter:
 
         # get the rest of the data
         self.mf = MFDataset(files)
-        for name in self.mf.variables.keys():
-            self.mf.variables[name][:] = \
-                np.ma.masked_greater(self.mf.variables['wind_v'][:,0], 9e+36)
 
-    def plot(self):
-        pass
+    def plot(self, variable_groups, begin=None, end=None, width=18, height=6):
+        '''public function for plotting a list of regex tuples/lists/sets
+        calls the private _plot method after creating all of the plot groups
+        in the correct format. The purpose of this function is two fold:
+            1.  Determine the number of plots that will be plotted
+            2.  Package all of the plots in a manner that is easy
+                to loop over and generate plots quickly
+        '''
+        plot_groups = []
 
+        for group in variable_groups:
+
+            var_names = {x for pattern in group \
+                for x in self.mf.variables.keys() \
+                if re.search(pattern, x)}
+
+            shapes = {self.mf.variables[x][:].shape for x in var_names}
+            if len(shapes) != 1:
+                print('Could not plot variables {} because of inconsistent shapes: {}'.format(
+                    ', '.join( 
+                        ['\'{}\''.format(x) for x in var_names]
+                    ),
+                    ', '.join(
+                        ['\'{}\''.format(self.mf.variables[x][:].shape) for x in var_names]
+                    )
+                ))
+                continue
+
+            dimensioned_by = {self.mf.variables[x].dimensions for x in var_names}
+            if len(dimensioned_by) != 1:
+                print('Could not plot variables {} because of inconsistent dimensions: {}'.format(
+                    ', '.join( 
+                        ['\'{}\''.format(x) for x in var_names]
+                    ),
+                    ', '.join(
+                        ['\'{}\''.format(self.mf.variables[x].dimensions) for x in var_names]
+                    )
+                ))
+                continue
+
+            shape = list(shapes)[0]
+            dimensions = len(shape)
+            if dimensions == 0:
+                plot_groups.append({
+                    'names': var_names,
+                    'dimensions': [],
+                    'slice': []
+                })
+
+            elif dimensions == 1:
+                plot_groups.append({
+                    'names': var_names,
+                    'dimensions': list(dimensioned_by)[0],
+                    'slice': [None]
+                })
+
+            elif dimensions == 2:
+                for column in range(shape[1]):
+                    plot_groups.append({
+                        'names': var_names,
+                        'dimensions': list(dimensioned_by)[0],
+                        'slice': [None, column]
+                    })
+
+            else:
+                print('3 dimensional matrix plots are not yet implemented')
+                continue
+
+        print(plot_groups)
+
+        self._plot(plot_groups, begin, end, width, height)
+
+    def _plot(self, plot_groups, begin, end, width, height):
+        '''actually plots the data
+        '''
+        num_plots = len(plot_groups)
+        fig = plt.figure(figsize=(width, height * num_plots))
+        colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+        # slice the time range
+        start_index = bin_search(self.time, begin) \
+            if begin else 0
+        end_index = bin_search(self.time, end) + 1 \
+            if end else self.time.size
+
+        for i, group in enumerate(plot_groups):
+            
+            dimensions = len(group['dimensions'])
+            ax = fig.add_subplot(num_plots, 1, i+1)
+            var_names = group['names']
+
+            for j, var_name in enumerate(var_names):
+                # plot each variable on the same graph
+                if dimensions == 0:
+                    ax.plot(
+                        np.ma.masked_greater(
+                            self.mf.variables[var_name][start_index:end_index],
+                            9e+36
+                        ),
+                        marker='o', color=colors[j % len(colors)]
+                    )
+
+                elif dimensions == 1:
+                    if 'time' in group['dimensions']:
+                        ax.plot(
+                            self.time[start_index:end_index],
+                            np.ma.masked_greater(
+                                self.mf.variables[var_name][start_index:end_index],
+                                9e+36
+                            ),
+                            marker='o', color=colors[j % len(colors)]
+                        )
+                    else:
+                        ax.plot(
+                            np.ma.masked_greater(
+                                self.mf.variables[var_name][:],
+                                9e+36
+                            ),
+                            marker='o', color=colors[j % len(colors)]
+                        )
+
+                elif dimensions == 2:
+                    if 'time' in group['dimensions']:
+                        ax.plot(
+                            self.time[start_index:end_index],
+                            np.ma.masked_greater(
+                                self.mf.variables[var_name][start_index:end_index][:,group['slice'][1]],
+                                9e+36
+                            ),
+                            marker='o', color=colors[j % len(colors)]
+                        )
+                    else:
+                        ax.plot(
+                            np.ma.masked_greater(
+                                self.mf.variables[var_name][:][:,group['slice'][1]],
+                                9e+36
+                            ),
+                            marker='o', color=colors[j % len(colors)]
+                        )
+
+                else:
+                    print('Not implemented yet')
+
+            # xlabels
+            if dimensions == 0:
+                ax.set(xlabel='data point')
+            elif dimensions == 1:
+                ax.set(xlabel=group['dimensions'][0])
+            elif dimensions == 2:
+                ax.set(xlabel=', '.join(group['dimensions']))
+            else:
+                print('not implemented')
+                pass
+
+            # everything else
+            # ax.set(
+            #     title=', '.join(var_names),
+            #    # ylabel=', '.join({self.mf.variables[x]['units'] for x in var_names})
+            # )
+
+            # ax.legend(['{} ({})'.format(x, self.mf.variables[x]['units'])\
+            #     for x in var_names
+            # ])
+
+        plt.show()
+
+
+# ----------------------------------------------------
+# - Helper Methods -----------------------------------
+# ----------------------------------------------------
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(b, a)
+
+def bin_search(a, d):
+    first = 0
+    last = len(a) - 1
+    found = False
+    m = -1
     
+    while first <= last and not found:
+        m = (first + last) // 2
+        if a[m] == d:
+            return m
+        else:
+            if a[m] > d:
+                last = m - 1
+            else:
+                first = m + 1
+    return m
